94c0a285a4a08a535a2f16d02298b3f30db10a12
quick007 <69743171+quick007@users.noreply.github.com>
2024-11-23 11:46:25 -0800

    turn back on middleware

--- middleware.ts
+++ middleware.ts
@@ -14,6 +14,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
      */
-    // "/dashboard/:path*",
+    "/dashboard/:path*",
   ],
 }
