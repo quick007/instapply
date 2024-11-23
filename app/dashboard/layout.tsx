ca81789109b30b816ac0e5ef47137c926b8cb8c1
quick007 <69743171+quick007@users.noreply.github.com>
2024-11-23 11:37:13 -0800

    profile button

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,13 +1,31 @@
 import Link from "next/link";
 import SubnavTab from "./apps/components/subnavTab";
 
-export default function Layout({children}: {children: React.ReactNode}) {
-    return (
-        <main>
-            <nav className="w-screen bg-blue-50 h-20 mb-5">
-                <img src = "/file.png" alt = "Logo Image" className="h-20"/>
-            </nav>
-            {children}
-        </main>
-    )
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <main>
+      <nav className="w-screen bg-blue-50 h-20 mb-5 flex justify-between items-center px-4">
+        <img src="/file.png" alt="Logo Image" className="h-20" />
+        <Link href="/dashboard/profile">
+          <div className="rounded-full p-2 bg-blue-200">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 text-blue-900"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+          </div>
+        </Link>
+      </nav>
+      {children}
+    </main>
+  );
 }
