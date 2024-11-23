7f1421680ca7ff1f2588213d42a361d858cc8846
quick007 <69743171+quick007@users.noreply.github.com>
2024-11-22 19:19:54 -0800

    fix tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  
+}
 
 export default function Home() {
   return (
     <div>
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-        <title>Classes</title>
-      </head>
       <main>
         home page here soon:tm: we nneed to do this
       </main>
