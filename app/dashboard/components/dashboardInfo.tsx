cce5536fa39a3d7e4b5ebb511b9acbe4b4955bba
quick007 <69743171+quick007@users.noreply.github.com>
2024-11-24 16:44:49 -0800

    see applications btn

--- app/dashboard/components/dashboardInfo.tsx
+++ app/dashboard/components/dashboardInfo.tsx
@@ -10,7 +10,6 @@ export default function DashboardInfo(
                 className="text-primary-verydark grow p-10 flex flex-col items-center justify-center gap-2 bg-primary-medium border-none rounded-3xl">
                 {children}
                 <h1 className="text-5xl">{count}</h1>
-                
             </Link>
         </div>
     );
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,16 +6,21 @@ import { getStatsData } from "./apps/components/getData";
 
 export default async function Page() {
   const supabase = await createClient();
-  const { data: { user }, error } = await supabase.auth.getUser();
-  const stats = await getStatsData()
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+  const stats = await getStatsData();
 
   if (error || !user) {
     return redirect("/");
   }
 
-  const { data, error: fetchError } = await supabase.from("profiles").select(
-    "id, made_profile, first_name",
-  ).eq("id", user.id).single();
+  const { data, error: fetchError } = await supabase
+    .from("profiles")
+    .select("id, made_profile, first_name")
+    .eq("id", user.id)
+    .single();
 
   if (fetchError || !data) {
     return redirect("/");
@@ -24,16 +29,27 @@ export default async function Page() {
   if (!data.made_profile) {
     return (
       <div className="flex flex-col items-center justify-center mt-44 bg-gradient-to-t from-primary-darkish to-primary-light">
-        <h1 className="font-semibold text-7xl flex mb-10">Create your profile</h1>
+        <h1 className="font-semibold text-7xl flex mb-10">
+          Create your profile
+        </h1>
         <div className="flex items-center justify-center">
           <img src="/businessMan.svg" className="h-72" />
           <Link href="/dashboard/profile">
             <div className="p-10 shadow-lg flex flex-col items-center rounded-full max-w-xl bg-primary-light animate-bounce">
-
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+                />
               </svg>
-
             </div>
           </Link>
         </div>
@@ -43,16 +59,41 @@ export default async function Page() {
 
   return (
     <div className="flex items-center justify-center grow bg-gradient-to-t from-primary-darkish to-primary-light">
-      <div className="flex flex-col items-center mb-20">
+      <div className="flex flex-col items-center mb-32">
         <h1 className="text-4xl font-semibold text-primary-verydark">
           Hello, {data.first_name}!
         </h1>
-        <h2 className="text-primary-verydark mb-10">Let{"'"}s continue with your applications!</h2>
+        <h2 className="text-primary-verydark mb-10">
+          Let{"'"}s continue with your applications!
+        </h2>
         <div className="flex justify-center gap-4 text-xl">
-          <DashboardInfo href="/dashboard/apps/recommended" count={stats!.rec}>Recommended</DashboardInfo>
-          <DashboardInfo href="/dashboard/apps/saved" count={stats!.sav}>Saved</DashboardInfo>
-          <DashboardInfo href="/dashboard/apps/applied" count={stats!.app}>Applied</DashboardInfo>
+          <DashboardInfo href="/dashboard/apps/recommended" count={stats!.rec}>
+            Recommended
+          </DashboardInfo>
+          <DashboardInfo href="/dashboard/apps/saved" count={stats!.sav}>
+            Saved
+          </DashboardInfo>
+          <DashboardInfo href="/dashboard/apps/applied" count={stats!.app}>
+            Applied
+          </DashboardInfo>
         </div>
+        <Link className="flex items-center mt-8 rounded-md bg-primary-darkish/30 font-medium text-white px-5 py-2 group shadow-sm" href="/dashboard/apps/recommended">
+          See applications
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6 ml-2 group-hover:translate-x-1 transition duration-300"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+            />
+          </svg>
+        </Link>
       </div>
     </div>
   );
